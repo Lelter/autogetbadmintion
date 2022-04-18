794851663ea8d0f47d7f2b6ccc68b6c14144fa80
vice-computer <1067049538@qq.com>
2022-04-18 14:49:28 +0800

    1

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from urllib import parse
 import requests
 import datetime
 import time
+import urllib3
+urllib3.disable_warnings()
+
 
 def main_handler(event, context):
     main()
@@ -11,10 +14,15 @@ def main_handler(event, context):
 
 def get_position(eachvalue, positions, start, end):
     sites = {1: "c7a1e6c77abb48c8b79e5da2bed939c7",
-             2: "",
+             2: "cc44395619064a2183e06105074a6b02",
              3: "e47431fbfd524876a03a1c5507d85beb",
              4: "9090387ea7b04591a73b1059114c0ab9",
-             5: "ac85e1a930464d0d8e30f129b661cfea", }
+             5: "282cd0b4130949838b7061722e840b56",
+             6: "ac85e1a930464d0d8e30f129b661cfea",
+             7: "10715133add844669aa2b238791dc65b",
+             8: "dd5f9b38bfb645c6a1c7ba1e22d26321",
+             9: "a48298dc3c3242c58af5b9c31d0d4795",
+             10: "2d3182fed841487caa4b62c677bf8f33"}
     day = (datetime.datetime.now() +
            datetime.timedelta(hours=8)).strftime('%Y-%m-%d')
     time_now = (datetime.datetime.now()+datetime.timedelta(hours=8)
@@ -42,11 +50,25 @@ def get_position(eachvalue, positions, start, end):
     total_Amount = payable = payment = each_value*len(detail_list)
     data = {"mainId": "09bcd2404fcb4af9951b38f03c2837db", "sportsType": "YuMaoQiu", "identity": "student", "billDateTime": time_now, "totalAmount": total_Amount, "payable": payable, "payment": payment,
             "detailedList": detail_list}
-    order_booking(data)
+    # start_post_time = datetime.datetime(2022, 4, 18, 14, 24, 0).time()
+    end_post_time = datetime.datetime(2022, 4, 17, 14,50, 0).time()
+    # print("start_post_time:", start_post_time)
+    print("end_post_time:", end_post_time)
+    count = 0
+    while True:
+        hour = datetime.datetime.now().time()
+        print("post")
+        print(hour)
+        order_booking(data)
+        count += 1
+        print("count:", count)
+        if hour >= end_post_time:
+            break
     pass
 
 
 def order_booking(data):
+    start = time.time()
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjA5YmNkMjQwNGZjYjRhZjk5NTFiMzhmMDNjMjgzN2RiIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IuWPtuWuh-a2myIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjE2NTA0NDM4MTYiLCJyZWZyZXNoX3RpbWUiOiIxNjUwMjI3ODE2IiwianRpIjoiMDliY2QyNDA0ZmNiNGFmOTk1MWIzOGYwM2MyODM3ZGIiLCJBcHBfUGVybWlzc2lvbnMiOiJBcHAiLCJuYmYiOjE2NTAwMTE4MTYsImV4cCI6MTY1MDQ0MzgxNiwiaXNzIjoic2VpbiIsImF1ZCI6InNlaW4ifQ.wXjiwePZzQX1dBRu2ZzhuiGXkTJ0l_YdPsu70DvDiww',
@@ -55,7 +77,8 @@ def order_booking(data):
         'Accept-Encoding': 'gzip, deflate, br'
     }
     url = 'https://wltyzx.cug.edu.cn/api/app/WeixinOrder/CreateOrder'
-    r = requests.post(url, json=data, headers=headers, verify=False)
+    r = requests.session().post(url, json=data, headers=headers, verify=False)
+    print("posttime:", time.time()-start)
     print(r.text)
     r = r.json()
     data = r['data']
@@ -70,10 +93,11 @@ def order_booking(data):
 
 
 def main():
-    start=time.time()
-    value = 20
-    start_time = 15
-    end_time = 17
-    position = [4, 5]
+    start = time.time()
+    value = 10
+    start_time = 19
+    end_time = 22
+    position = [5]
     get_position(value, position, start_time, end_time)
-    print("time:",time.time()-start)
+    print("time:", time.time()-start)
+main()
